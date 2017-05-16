64330a7255b2cd5435dd26d8520c1ff0c1badc48
Tyler <tyaramer@gmail.com>
2017-05-16 17:18:17 -0400

    t

---
+++ ksedpleh.cpp
@@ -0,0 +1,10 @@
+/* KSEPLEH
+
+TALK TO THE HELP DESK
+Quick automated analyis of common problems
+
+Will take inputs, as well as intelligently make suggestions.
+
+in the future, will it be modular????
+
+*/
