c5ff66dcefe6965bfcef960d716116d1583945fd
Tyler <tyaramer@gmail.com>
2017-05-22 16:31:17 -0400

    adding more changes

--- ksedpleh.sh
+++ ksedpleh.sh
@@ -6,21 +6,8 @@ OPTIND=1
 show_help() {
   cat <<EOF
   Usage: ${0##*/} [-options]
-  A tool for the kernel team for analyzing sosreports intelegently
+  A tool for the kernel team for analyzing sosreports inteligently
 EOF
-}
-
-
-kdump_check() {
-
-}
-
-reboot_rca() {
-
-}
-
-oracle_tune() {
-
 }
 
 
@@ -30,10 +17,52 @@ then
   exit 1
 fi
 
+if [ ! -e etc/redhat-release ]
+then
+  echo "Must be run in sosreport directory"
+  exit 1
+fi
+
+
+
+# setting some variables
+
+
+kdump_check() {
+  echo
+}
+
+reboot_rca() {
+  echo
+
+if ! grep mockbuild var/log/messages*
+then
+  echo "I do not see a record of recent reboots in the included logs. Please ensure that
+ all relevant logs are included in the case. Feel free to create a tar file of all
+ relevant logs and upload them to the case
+
+$ grep mockbuild/var/log/messsages* | wc -l
+0"
+else
+  echo "I see these instances of recent reboots:"
+  echo '$grep mockbuild var/log/messages*'
+  grep mockbuild var/log/messages*
+fi
+
+}
+
+oracle_tune() {
+  echo
+}
+
+
+
+
+
 
 # inputs
 
-while getopts "" opt; do
+while getopts "hkro" opt; do
   case "$opt" in
 
   h)
